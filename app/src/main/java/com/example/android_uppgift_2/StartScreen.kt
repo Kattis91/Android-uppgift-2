a8f5d3cf6ce16096903732df6a69108ab0d12bef
Kattis91 <katya.svedmark@gmail.com>
2025-03-09 16:43:59 +0100

    Add logic to the plus button (count + 1)

--- app/src/main/java/com/example/android_uppgift_2/StartScreen.kt
+++ app/src/main/java/com/example/android_uppgift_2/StartScreen.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,16 +26,18 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun StartScreen() {
 
+    var count by remember { mutableStateOf(0) }
+
     Column(
         modifier = Modifier.fillMaxSize().background(Color.Cyan),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
 
-        Text("1", fontSize = 100.sp)
+        Text("$count", fontSize = 100.sp)
 
         Button(onClick = {
-
+            count += 1
         },
             modifier = Modifier
                 .fillMaxWidth()
